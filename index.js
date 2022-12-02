ab5346833e22a1309be82e6f7c6be66f18a9f476
ngachadavid <davidngacha01@gmail.com>
2022-12-02 20:12:43 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+
+const superbowlWin = collection => {
+    for (const item of collection) {
+        if (item.result==='W') {
+            return item.year
+        }
+    }
+}
